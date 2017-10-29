045f3510fb59380019826a2333c4bfcca67fce6e
fan807017570 <807017570@qq.com>
2017-10-29 21:38:33 +0800

    first commit

---
+++ src/main/TaskOfCron.py
@@ -0,0 +1,28 @@
+import os
+import yaml
+import urllib2
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+DEFAULT_PATH = "../resources/"
+
+
+def job():
+    print "hello this is the first job!!!!"
+
+
+def readYaml():
+    configDict = {}
+    filenames = os.listdir(DEFAULT_PATH)
+    for file in filenames:
+        configDict = yaml.load(open(DEFAULT_PATH + "/" + file))
+        print configDict
+    return configDict
+
+
+print readYaml()
+scheduler = BlockingScheduler()
+scheduler.add_job(job, 'cron', second=readYaml()['pl-a-batch'], hour='*')
+try:
+    scheduler.start()
+except(KeyboardInterrupt, SystemExit):
+    scheduler.shutdown()
